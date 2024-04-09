a91d2f9888d3fef1b27a29179a1457b8e2feb257
DavidELBAZpro <david.elbaz.pro@gmail.com>
2024-04-09 14:45:21 +0200

    Clients and products controllers and modules added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WelcomeController } from './welcome/welcome.controller';
+import { ProductsModule } from './products/products.module';
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, ClientsModule],
   controllers: [AppController, WelcomeController],
   providers: [AppService],
 })
