5dfc21f0c82cd4aa5a43614adf9695db9715f3fb
DavidELBAZpro <david.elbaz.pro@gmail.com>
2024-04-11 19:27:50 +0200

    404 Page implemented

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
 import ProductsPage from '../pages/ProductsPage.vue'
 import WelcomePage from '../pages/WelcomePage.vue'
+import NotFoundPage from '../pages/NotFoundPage.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/',
     name: 'WelcomePage',
     component: WelcomePage,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFoundPage',
+    component: NotFoundPage,
   },
   // Ajouter d'autres routes ici
 ]
