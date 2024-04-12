5d69cce838d5e5a58ab83670206fe61b9eeed0fb
DavidELBAZpro <david.elbaz.pro@gmail.com>
2024-04-12 19:01:54 +0200

    Fix modal style tlwd

--- frontend/src/components/AddProduct.vue
+++ frontend/src/components/AddProduct.vue
@@ -3,7 +3,7 @@
     @click="isModalOpen = true"
     data-modal-target="crud-modal"
     data-modal-toggle="crud-modal"
-    class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    class="block float-right text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
     type="button"
   >
     + Ajouter un produit
@@ -15,9 +15,10 @@
     id="crud-modal"
     tabindex="-1"
     aria-hidden="true"
-    class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+    class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-auto z-50 flex justify-end items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+    @click="closeOnOutsideClick"
   >
-    <div class="relative p-4 w-full max-w-md max-h-full">
+    <div class="relative p-4 w-full max-w-md max-h-full" @click.stop>
       <!-- Modal content -->
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
         <!-- Modal header -->
@@ -185,6 +186,13 @@ const imageData = reactive({
   file: null as File | null,
   url: '' as string,
 })
+
+const closeOnOutsideClick = (event: MouseEvent) => {
+  if (event.target instanceof HTMLElement && event.target.id === 'crud-modal') {
+    isModalOpen.value = false
+  }
+}
+
 function handleFileUpload(event: Event) {
   const files = (event.target as HTMLInputElement).files
   if (!files) return
