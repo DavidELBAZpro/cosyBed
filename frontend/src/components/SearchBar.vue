e2d8c7615b74737f35444296e35c358e30cbd092
DavidELBAZpro <david.elbaz.pro@gmail.com>
2024-04-18 12:18:31 +0200

    SearchBar types fixed

--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -28,19 +28,20 @@
         type="search"
         id="default-search"
         class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Search product by name..."
+        placeholder="Search product by name or id..."
         required
-        @input="emitSearch($event.target.value)"
+        @input="emitSearch($event)"
       />
     </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { defineEmits } from 'vue'
 const emit = defineEmits(['onSearch'])
 
-const emitSearch = (value) => {
-  emit('onSearch', value)
+const emitSearch = (event: Event | null) => {
+  const element = event?.currentTarget as HTMLInputElement
+  emit('onSearch', element.value)
 }
 </script>
