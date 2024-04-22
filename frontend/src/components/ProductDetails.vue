bd5e6d7ab6bd55df83b21ced012671839d6a686d
DavidELBAZpro <david.elbaz.pro@gmail.com>
2024-04-22 20:37:02 +0200

    Modal UI update

--- frontend/src/components/ProductDetails.vue
+++ frontend/src/components/ProductDetails.vue
@@ -5,19 +5,25 @@
     tabindex="-1"
     aria-hidden="true"
     class="fixed inset-0 overflow-y-auto overflow-x-hidden z-50 flex justify-center items-center h-[calc(100%-1rem)] max-h-full"
+    :class="{
+      'opacity-0': !isVisible,
+      'opacity-100 animate-scale-up-bl': isVisible,
+    }"
   >
     <div
       class="relative p-4 w-full max-w-2xl max-h-full bg-white rounded-lg shadow dark:bg-gray-700"
     >
+      <!-- Image ajustée avec object-contain -->
       <img
-        class="rounded-t-lg product-image w-full"
+        class="rounded-t-lg w-full max-h-full object-contain"
         :src="product.imageUrl"
         :alt="product.name"
       />
+      <!-- Bouton positionné -->
       <button
         @click="closeModal"
         type="button"
-        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+        class="absolute top-3 right-3 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
       >
         <svg
           class="w-3 h-3"
@@ -35,26 +41,22 @@
         </svg>
         <span class="sr-only">Close modal</span>
       </button>
-      <div
-        class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600"
-      ></div>
-      <div
-        class="flex items-center justify-between p-4 md:p-5 border-b border-gray-200 dark:border-gray-600"
-      >
-        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+      <div class="border-t border-gray-200 dark:border-gray-600">
+        <h3 class="p-4 text-xl font-semibold text-gray-900 dark:text-white">
           {{ product.name }}
         </h3>
-      </div>
-      <div class="p-4 md:p-5 space-y-4">
-        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+        <p
+          class="px-4 pb-4 text-base leading-relaxed text-gray-500 dark:text-gray-400"
+        >
           {{ product.description }}
         </p>
       </div>
     </div>
   </div>
 </template>
+
 <script setup>
-import { defineProps, defineEmits } from 'vue'
+import { defineEmits } from 'vue'
 
 const props = defineProps({
   product: Object,
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,21 @@ export default {
     extend: {
       screens: {
         'xxl': '1440px' // Ajouter un breakpoint personnalisé 'xxl'
+      },
+      keyframes: {
+        'scale-up-bl': {
+          '0%': {
+            transform: 'scale(0.5)',
+            transformOrigin: 'bottom left'
+          },
+          '100%': {
+            transform: 'scale(1)',
+            transformOrigin: 'bottom left'
+          }
+        }
+      },
+      animation: {
+        'scale-up-bl': 'scale-up-bl 0.3s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'
       }
     },
   },
