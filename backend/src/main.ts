ed4d256c9d2cda0073576579b448a8aa82b0ee89
DavidELBAZpro <david.elbaz.pro@gmail.com>
2024-05-02 18:17:45 +0200

    NODE_ENV & .dockerignore added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   // Activer CORS
   app.enableCors()
-  await app.listen(1605, '0.0.0.0')
+  await app.listen(
+    (process.env.NODE_ENV = 'production' ? '1605' : '1606'),
+    '0.0.0.0',
+  )
 }
 bootstrap()
